533d8ac875626c2fc1c14d87844d1dc7d85d8946
kamilsotollc-ctrl <kamilsotollc@gmail.com>
2026-07-17 21:35:37 -0400

    Delete test_hello.py

--- test_hello.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-
-
-def test_build_artifact_exists():
-    artifact = pathlib.Path("build") / "hello.txt"
-    assert artifact.exists(), "build_hello.py should have created build/hello.txt"
-    assert "buildgen" in artifact.read_text()
